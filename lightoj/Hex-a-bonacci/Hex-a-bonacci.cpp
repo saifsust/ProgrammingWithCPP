b4b09bb8d876f634791427249c1c35a9d09ddc2e
Saiful Islam <saifsust@users.noreply.github.com>
2018-07-20 17:58:34 +0600

    Hex-a-bonacci

---
+++ lightoj/Hex-a-bonacci/Hex-a-bonacci.cpp
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+int arr[10002];
+int a, b, c, d, e, f;
+int fn( int n )
+{
+    if( n == 0 ) return a;
+    if( n == 1 ) return b;
+    if( n == 2 ) return c;
+    if( n == 3 ) return d;
+    if( n == 4 ) return e;
+    if( n == 5 ) return f;
+    if(arr[n] != -1)return arr[n];
+    return arr[n]= ( fn(n-1)% 10000007+ fn(n-2)% 10000007 + fn(n-3)% 10000007 + fn(n-4)% 10000007 + fn(n-5)% 10000007 + fn(n-6)% 10000007 )% 10000007;
+}
+int main()
+{
+    int n, caseno = 0, cases;
+    scanf("%d", &cases);
+    while( cases-- )
+    {
+        scanf("%d %d %d %d %d %d %d", &a, &b, &c, &d, &e, &f, &n);
+        memset(arr,-1,sizeof(arr));
+        printf("Case %d: %d\n", ++caseno, fn(n) % 10000007);
+    }
+}
